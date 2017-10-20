07bea6ff1277acf9eec6ffc168a9ef1ea28a405e
ykuan <ykuan1992@163.com>
2017-10-21 01:57:25 +0800

    modified config/db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yiisoft',
     'username' => 'root',
-    'password' => 'yangkuan1992',
+    'password' => 'brokenyouth1992',
     'charset' => 'utf8',
 ];
